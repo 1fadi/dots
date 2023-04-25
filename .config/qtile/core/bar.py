c496bfe6bddc16684e5f64450bec6b0bf2d6362f
1fadi <1fadi@proton.me>
2023-04-25 15:32:30 +0200

    made some changes.

--- .config/qtile/core/bar.py
+++ .config/qtile/core/bar.py
@@ -1,27 +1,29 @@
 from libqtile import bar, widget
 from libqtile.lazy import lazy
 
-from .qwidgets import ClickableClock, VPN, Battery, Volume
+from .qwidgets import *
 from .helpers import shorten_window_name
 
 from utils import cpu as cpu_func
 
 bar_1 = bar.Bar([
-    widget.TextBox(
-        "",
-        foreground="#809fff",
-        fontsize=35,
-        # background="292f36",
-        padding=5,
-        mouse_callbacks={
-            'Button3': lazy.spawn('shutdown -P +1'),
-            'Button1': lazy.spawn('shutdown -c')
-        }),
+    # widget.TextBox(
+    #     "",
+    #     foreground="#809fff",
+    #     fontsize=35,
+    #     # background="292f36",
+    #     padding=5,
+    #     mouse_callbacks={
+    #         'Button3': lazy.spawn('shutdown -P +1'),
+    #         'Button1': lazy.spawn('shutdown -c')
+    #     }),
     widget.GroupBox(
         highlight_method="block",
         this_current_screen_border="#809fff",
         fontsize=17,
         hide_unused=False,
+        fmt="{}",
+        padding=8
     ),
     widget.Prompt(),
     widget.WindowName(
@@ -37,18 +39,13 @@ bar_1 = bar.Bar([
     ClickableClock(
         name="clock2",
         time_format="%I:%M %p",
-        date_format="%a, %B %d, %Y",
+        date_format="%a, %d.%m.%Y %I:%M %p",
         fmt=" {} ",
         # background="292f36"
     ),
     widget.Spacer(length=bar.STRETCH),
     widget.Systray(),
     widget.WidgetBox(widgets=[
-        VPN(
-            on="VPN",
-            update_interval=2,
-            fmt=" {} "
-        ),
         # widget.Sep(
         #     padding=1,
         #     linewidth=1,
@@ -76,33 +73,28 @@ bar_1 = bar.Bar([
             update_interval=2,
             fmt=" {} "
         ),
-
-        widget.Sep(
-            padding=5,
-            linewidth=2,
-        ),
-    ], text_closed=" ", text_open=" ", fontsize=26),
-    # Volume(
-    #     emoji=True,
-    #     fontsize=35,
-    #     padding=2,
-    #     foreground="872357"
-    # ),
-    Battery(notify=True, update_interval=10),
-    # widget.Battery(
-    #     charge_char='',
-    #     full_char='',
-    #     empty_char='',
-    #     discharge_char='',
-    #     unknown_char='',
-    #     update_interval=30,
-    #     format='{char} {percent:2.0%}',
-    #     background="273f1e",
-    #     fmt=" {} "
-    # ),
+    ],
+        text_closed="",
+        text_open="",
+        fontsize=25,
+        close_button_location="right"
+    ),
+    VPN(
+        on="VPN",
+        update_interval=2,
+        padding=6,
+        fontsize=12
+    ),
+    Volume(foreground="d5d5d5", size=18),
+    Network(size=13, interfaces=["wlan0", "enp0s31f6"]),
+    Battery(
+        notify=True,
+        update_interval=10,
+        size=(16, 32)
+    ),
 ],
-    24,
-    background="4d4d4d",
+    25,
+    background="1a1a1a",
     border_width=[0, 1, 1, 1],
     border_color=["000000", "000000", "000000", "000000"],
 )
